37e9bbf7505473977d0c8f882dbb35fa819b2191
programmer <jaredob2@gmail.com>
2017-10-21 14:40:51 +0100

    end

--- game.js
+++ game.js
@@ -7,3 +7,12 @@ cookieElement.addEventListener("click", function()
   scoreElement.textContent = score + " targets hit";
 });
 
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+}
